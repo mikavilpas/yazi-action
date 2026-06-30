1d6a0bf0c2822cf51606b2bd8d0d60e3d37a86dd
Mika Vilpas <mika.vilpas@gmail.com>
2026-06-30 18:45:44 +0300

    ci: renovate updates yazi commit from main branch, not master
    https://developer.mend.io/github/mikavilpas/yazi-action/-/job/1937b5a5-a88a-4304-99fd-7667231d8182
    
    ```json
    {
      "regex": [
        {
          "deps": [
            {
              "packageName": "https://github.com/sxyazi/yazi",
              "currentValue": "master",
              "currentDigest": "21550a7eb5086ba34f8eabf8aaab85f601d34a74",
              "datasource": "git-refs",
              "replaceString": "# renovate: datasource=git-refs-master packageName=https://github.com/sxyazi/yazi\n  YAZI_NIGHTLY_COMMIT: 21550a7eb5086ba34f8eabf8aaab85f601d34a74",
              "depName": "https://github.com/sxyazi/yazi",
              "updates": [],
              "versioning": "semver-coerced",
              "warnings": [
                {
                  "message": "Could not determine new digest for update (git-refs package https://github.com/sxyazi/yazi)",
                  "topic": "https://github.com/sxyazi/yazi"
                }
              ]
            }
          ],
          "matchStrings": [
            "#\\s*renovate:\\s*datasource=git-refs-master\\s+packageName=(?<packageName>\\S+)\\s*[\\r\\n]+[^:]+:\\s*\"?(?<currentDigest>[^\",\\s]+)\"?",
            "-- renovate: datasource=git-refs-master packageName=(?<packageName>[^\\s]+)\\s*?commit = \"?(?<currentDigest>[^\",]+)\"?"
          ],
          "currentValueTemplate": "master",
          "datasourceTemplate": "git-refs",
          "packageFile": ".github/workflows/ci.yml"
        }
      ]
    }
    ```

---
+++ commitlint.config.ts
@@ -0,0 +1,13 @@
+import type { UserConfig } from "@commitlint/types";
+import { RuleConfigSeverity } from "@commitlint/types";
+
+// https://commitlint.js.org/
+const config: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "body-max-line-length": [RuleConfigSeverity.Disabled],
+    "footer-max-line-length": [RuleConfigSeverity.Disabled],
+  },
+};
+
+export default config;
